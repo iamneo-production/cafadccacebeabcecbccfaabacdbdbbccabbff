a1f9ea24d5f071e894413d8a0a2e35145bde4b4e
Iamneo <techadmin@iamneo.ai>
2023-08-13 14:28:58 +0000

    Backup Commit

--- CucumberBasics/src/test/java/stepDefinitions/OpenGoogleDefinitions.java
+++ CucumberBasics/src/test/java/stepDefinitions/OpenGoogleDefinitions.java
@@ -1,17 +1,15 @@
 package stepDefinitions;
 
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-
-import java.util.List;
-
+import org.junit.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.Select;
-
-import cucumber.api.DataTable;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 
@@ -20,49 +18,55 @@ import cucumber.api.DataTable;
 class OpenGoogleDefinitions
 {
 
-    WebDriver driver = null;
-    @Given("^I am on new user registration page$") 
-    public void goToFacebook() { 
-       //Intiate web browser instance. driver = new FirefoxDriver();
-  driver.navigate().to("https://www.facebook.com/"); 
-    } 
-  
-    @When("^I enter invalid data on the page$") 
-    public void enterData(DataTable table){ 
-       //Initialize data table 
-       List<List<String>> data = table.raw();
-       System.out.println(data.get(1).get(1)); 
-       
-       //Enter data
-       driver.findElement(By.name("firstname")).sendKeys(data.get(1).get(1));
-       driver.findElement(By.name("lastname")).sendKeys(data.get(2).get(1));
-       driver.findElement(By.name("reg_email__")).sendKeys(data.get(3).get(1));     
-       driver.findElement(By.name("reg_email_confirmation__")).
-          sendKeys(data.get(4).get(1)); 
-       driver.findElement(By.name("reg_passwd__")).sendKeys(data.get(5).get(1)); 
-       
-       Select dropdownB = new Select(driver.findElement(By.name("birthday_day"))); 
-       dropdownB.selectByValue("15"); 
-   
-       Select dropdownM = new Select(driver.findElement(By.name("birthday_month")));
-       dropdownM.selectByValue("6"); 
-   
-       Select dropdownY = new Select(driver.findElement(By.name("birthday_year")));
-       dropdownY.selectByValue("1990"); 
-   
-       driver.findElement(By.className("_58mt")).click(); 
-  // Click submit button driver.findElement(By.name("websubmit")).click(); 
-    } 
-  
-    @Then("^User registration should be unsuccessful$") 
-    public void User_registration_should_be_unsuccessful() {
-       if(driver.getCurrentUrl().equalsIgnoreCase("https://www.facebook.com/")){
-          System.out.println("Test Pass"); 
-       } else { 
-          System.out.println("Test Failed"); 
-       } 
-       driver.close(); 
-    } 
+    WebDriver Driver;
+    private final Logger logger = LoggerFactory
+            .getLogger(newuser_StepDefinitions.class);
+
+
+    @Given("^user on the user registration page$")
+    public void Setup()
+    {
+        WebDriverManager.chromedriver().setup();
+        Driver=new ChromeDriver();
+        Driver.get("https://www.google.com/aclk?sa=l&ai=DChcSEwj61pnn_v_8AhULMSsKHXOzCpUYABAAGgJzZg&sig=AOD64_09wu1CmKVSAOWwQdnCTDG-62t4xg&q&adurl&ved=2ahUKEwjYu5Tn_v_8AhXnZWwGHfMCAH0Q0Qx6BAgIEAE");
+        logger.info("Page for Facebook had Navigated");
+        Driver.findElement(By.xpath("//*[@id="u_0_b_5q"]")).sendKeys("ABC");
+        Driver.findElement(By.xpath("//*[@id="u_0_d_89"]")).sendKeys("XYZ");
+        logger.info("User enter the Valid UserName");
+
+    }
+    @And("^valid e-mail address$")
+    public void mail() {
+        Driver.findElement(By.xpath("//*[@id="u_0_g_iL"]")).sendKeys("test@test.com");
+        logger.info("E-mail has been Entered and validate");
+    }
+    @And("^valid password$")
+    public void mailpassword() {
+        Driver.findElement(By.xpath("//*[@id="password_step_input"]")).sendKeys("Password@123");
+        logger.info("E-mail Password  has been Entered and validate");
+    }
+    @And("^valid Birth-date$")
+    public void dob() {
+        Select date = new Select(Driver.findElement(By.xpath("//*[@id="day"]")));
+        date.selectByVisibleText("1");
+        Select month = new Select(Driver.findElement(By.xpath("//*[@id="month"]")));
+        month.selectByVisibleText("1");
+        Select year = new Select(Driver.findElement(By.xpath("//*[@id="year"]")));
+        year.selectByVisibleText("2001");
+        logger.info("Birth Date has been validate");
+    }
+    @And("^valid Gender$")
+    public void Gender() {
+        Driver.findElement(By.xpath("//*[@id="u_0_o_tb"]/span[2]/label"]")).click();
+        logger.info("Gender has been Entered and validate");
+    }
+    @Then("^user registration should be successful $")
+    public void validate() {
+        Driver.findElement(By.xpath("//*[@id="u_0_s_+5"]")).click();
+        logger.info("User registration should be successful");
+
+    }
+
 
 }
 
